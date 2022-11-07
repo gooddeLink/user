70222706eed9d8398c74abae4c6f8a332bb65265
esthel <esthel9026@naver.com>
2022-11-07 22:34:44 +0900

    design user

--- 코드/gooddeLink/user/src/App.js
+++ 코드/gooddeLink/user/src/App.js
@@ -5,7 +5,6 @@ import Home from "./routes/Home";
 function App() {
   //Routes는 하나의 Route 만 실행되도록 함
   // '/search'가 '/'보다 앞에 있어야 함. /search는 /속에도 속하기 때문
-  
   return (
     <BrowserRouter>
       <Routes>
