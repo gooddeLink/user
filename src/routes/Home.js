972c383014347348587119e7bf325163125e7688
dahyunko <kdhgby@naver.com>
2022-11-10 16:56:02 +0900

    all done expected

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -43,10 +43,14 @@ function Home(){
     var [styleCam2, styleSetCam2]=useState({display:'none'});
 
     var [file, setFile]=useState();//react 내에서 값 바꾸기 위하여 useState 이용해서 변수 선언
-    var [imgUrl, setImgUrl]=useState();
+    var [imgBlob, setImgBlob]=useState();
+    var [dataUrl, setDataUrl]=useState();
+    var [selectedValue, setSelectedValue]=useState();
 
     var latestFile=useRef(file);//바뀐 값 사용하기 위함
-    var latestImgUrl=useRef(imgUrl);
+    var latestImgBlob=useRef(imgBlob);
+    var latestDataUrl=useRef(dataUrl);
+    var latestSelectedValue=useRef(selectedValue);
 
     //취약계층 위치신고 : a
     //재난위험 사진신고 : b
@@ -68,25 +72,28 @@ function Home(){
                 };
                 setMap = new google.maps.Map(document.getElementById("googleMap"), mapOptions );
                 latestMap.current=setMap;
-                
-                setMarker = new google.maps.Marker({position: {lat: setLat, lng: setLon}, map: latestMap.current});
-                latestMarker.current=setMarker;
 
                 setGeocoder=new google.maps.Geocoder();//주소를 위도 경도로 변환
                 latestGeocoder.current=setGeocoder;
 
                 //주소 자동완성기능
                 if(flag==='a'){
+                    var myIcon = new google.maps.MarkerImage("../../picture/pin.png", null, null, null, new google.maps.Size(66,88));
                     var autocomplete = new google.maps.places.Autocomplete(document.getElementById('loadaddrLoc'), {
                         types: ['geocode']
                     });
                 }
                 else{
+                    var myIcon = new google.maps.MarkerImage("../../picture/orangepin.png", null, null, null, new google.maps.Size(66,88));
                     var autocomplete = new google.maps.places.Autocomplete(document.getElementById('loadaddrPic'), {
                         types: ['geocode']
                     });
                 }
                 autocomplete.addListener('place_changed');
+
+                setMarker = new google.maps.Marker({position: {lat: setLat, lng: setLon}, map: latestMap.current, icon: myIcon});
+                //icon 항목은 기존의 marker 이미지 변경
+                latestMarker.current=setMarker;
 
                 styleSetLoading({display:'none'});//지도가 로딩되면 가리기
                 styleSetMap({display:'block'});//지도가 로딩되면 홈페이지 나타나도록
@@ -110,7 +117,7 @@ function Home(){
                 //클릭하면 마커 변화
                 google.maps.event.addListener(latestMap.current, 'click', function(event){
                     latestMarker.current.setMap(null);//마커 하나만 뜨도록 기존것 없애주기
-                    latestMarker.current=new google.maps.Marker({position: {lat: event.latLng.lat(), lng: event.latLng.lng()}, map: latestMap.current});
+                    latestMarker.current=new google.maps.Marker({position: {lat: event.latLng.lat(), lng: event.latLng.lng()}, map: latestMap.current, icon: myIcon});
                     latlon2addr(event.latLng.lat(),event.latLng.lng(),latestMarker.current);//위도경도를 주소로 변환
                     latestMap.current.setCenter(latestMarker.current.getPosition());//마커가 가운데 위치하도록
                     latestMarker.current.setMap(latestMap.current);
@@ -124,7 +131,9 @@ function Home(){
         // var geocoder=new google.maps.Geocoder();//주소를 위도 경도로 변환
         latestGeocoder.current.geocode({'location':{lat: lat, lng: lon}},function(results,status){
             if (status==='OK') {
-                makebox(results[0].formatted_address,latestMap.current);//주소표시
+                setLoc=results[0].formatted_address;
+                latestLoc.current=setLoc;
+                showAddr(latestLoc.current);//주소창에 주소표시
             }
             else alert('error address');
         });
@@ -141,21 +150,6 @@ function Home(){
             print.innerHTML='<img src="../../picture/orangepin.png" class="pin" alt="loc mark" />'+loc;
         }
         //className말고 class로 설정
-    }
-
-    //infowindow에 주소 표시하기
-    function makebox(addr){
-        setLoc=addr;
-        latestLoc.current=setLoc;
-        var detail='<div class="roadAddr">도로명주소 : '+addr+'</div>';
-        var content='<div class="detailAddr"><span class="addr">법정동 주소정보</span>'
-            +detail+'</div>';
-        
-        var infowindow=new google.maps.InfoWindow();
-        infowindow.setContent(content);
-        infowindow.open(latestMap.current, latestMarker.current);
-
-        showAddr(latestLoc.current);//주소창에 주소표시
     }
 
     function textCheck(){//글자수 제한
@@ -186,10 +180,12 @@ function Home(){
 
     //재난유형 select option 선택하기
     function optionSelect(event){
-        var selectedValue = event.currentTarget.innerHTML;//선택한 항목 글자
+        setSelectedValue = event.currentTarget.innerHTML;//선택한 항목 글자
+        latestSelectedValue.current=setSelectedValue;
+
         setType=event.currentTarget.value;//재난유형 번호
         latestType.current=setType;
-        document.querySelector(".base").innerHTML = selectedValue;//선택한 항목 글자가 나타나게 함
+        document.querySelector(".base").innerHTML = latestSelectedValue.current;//선택한 항목 글자가 나타나게 함
     }
 
     //카메라 실행
@@ -203,56 +199,40 @@ function Home(){
                 
                 try{//오류있을수도 있는 문장
                     var link=window.URL||window.webkitURL;//window.URL 객체 얻기
-                    setImgUrl=link.createObjectURL(latestFile.current);//objectURL 생성
-                    latestImgUrl.current=setImgUrl;//useEffect 밖에서도 사용하기 위함
+                    setImgBlob=link.createObjectURL(latestFile.current);//objectURL 생성
+                    latestImgBlob.current=setImgBlob;//useEffect 밖에서도 사용하기 위함
+
+                    //dataurl로 변환
+                    var reader=new FileReader();
+                    reader.readAsDataURL(latestFile.current);
+                    reader.onload=function(event){
+                        setDataUrl=event.target.result;
+                        latestDataUrl.current=setDataUrl;
+                    }
 
                     if(flag==='a'){
                         var show2=document.getElementById('show2');
-                        show2.src=latestImgUrl.current;//사진 주소 설정
+                        show2.src=latestImgBlob.current;//사진 주소 설정
                         styleSetCamImg2({display:'none'});//사진 선택할 이미지로고 안보이게
                         styleSetImgShow2({display:'block'});//선택한 사진 보이게
-
-                        axios.post(`http://127.0.0.1:5000/${id}/locimgsubmit`, {//정보 전달할 페이지-ai
-                            img:latestImgUrl.current
-                        })
-                        .then((res)=>{//axios.post 성공하면
-                            console.log(res);
-                        })
-                        .catch((err)=> {//axios.post 에러나면
-                            console.log(err);
-                            alert(`오류가 발생했습니다.\n${err.message}`);
-                            return;
-                        });
                     }
                     if(flag==='b'){
                         var show1=document.getElementById('show1');
-                        show1.src=latestImgUrl.current;//사진 주소 설정
+                        show1.src=latestImgBlob.current;//사진 주소 설정
                         styleSetCamImg1({display:'none'});//사진 선택할 이미지로고 안보이게
                         styleSetImgShow1({display:'block'});//선택한 사진 보이게
-
-                        axios.post(`http://127.0.0.1:5000/${id}/camimgsubmit`, {//정보 전달할 페이지-ai
-                            img:latestImgUrl.current
-                        })
-                        .then((res)=>{//axios.post 성공하면
-                            console.log(res);
-                        })
-                        .catch((err)=> {//axios.post 에러나면
-                            console.log(err);
-                            alert(`오류가 발생했습니다.\n${err.message}`);
-                            return;
-                        });
                     }
 
                     var revoke=document.querySelector('.boxgroup');
-                    revoke.onload=function(){link.revokeObjectURL(imgUrl);}//이미지 띄우고 url 취소하기(메모리 절약 위함)
+                    revoke.onload=function(){link.revokeObjectURL(imgBlob);}//이미지 띄우고 url 취소하기(메모리 절약 위함)
                 }
                 catch(e){//에러 있다면
                     console.log('error');
                     try{
                         var fileReader=new FileReader();//createObject가 안되는 경우
                         fileReader.onload=function(event){
-                            setImgUrl=event.target.result;//useEffect 밖에서도 사용하기 위함
-                            latestImgUrl.current=setImgUrl;
+                            setImgBlob=event.target.result;//useEffect 밖에서도 사용하기 위함
+                            latestImgBlob.current=setImgBlob;
                         };
                         fileReader.readAsDataURL(file);
                     }
@@ -329,6 +309,12 @@ function Home(){
 
     //서비스 이용완료
     function Finish(){
+        if(flag==='b'){//재난 유형, 현장 사진 접수 입력 안했다면 alert
+            if(!latestSelectedValue.current || !latestDataUrl.current) {
+                alert('필수 항목이 누락되었습니다.');
+                return;
+            }
+        }
         styleSetModal({display:'flex'});//가운데 오도록
 
         if(flag==='a'){
@@ -348,7 +334,7 @@ function Home(){
             });
         }
         else{
-            axios.post(`${process.env.REACT_APP_goodde}/call_cam/${id}/submit`, {//정보 전달할 페이지-admin
+            axios.post(`${process.env.REACT_APP_goodde}/call_loc/${id}/submit`, {//정보 전달할 페이지-admin
                 type:latestType.current,//유형번호
                 text:latestMsg.current,//불편내용
                 lat:latestLat.current,
@@ -364,6 +350,18 @@ function Home(){
                 return;
             });
         }
+
+        axios.post(`http://127.0.0.1:5000/${id}/locimgsubmit`, {//정보 전달할 페이지-ai
+            img:latestDataUrl.current
+        })
+        .then((res)=>{//axios.post 성공하면
+            console.log(res);
+        })
+        .catch((err)=> {//axios.post 에러나면
+            console.log(err);
+            alert(`오류가 발생했습니다.\n${err.message}`);
+            return;
+        });
     }
 
     return (
@@ -378,7 +376,7 @@ function Home(){
 
         <div className="boxgroup" style={styleCamHead}> {/*재난유형 사진신고*/}
             <div className="write" style={styleColor}>
-                재난 유형<span className="chooseType">(필수*)</span>
+                재난 유형<span className="chooseType" style={{color:'#3ac47d'}}>(필수*)</span>
             </div>
             <div className="typeGroup" onClick={Select}>
                 <div className="base">
@@ -399,15 +397,19 @@ function Home(){
             {/* 취약 이웃 위치 신고 - camera1 */}
             <div style={styleCam1}>
                 <div className="write" style={styleColor}>
-                    현장 사진 접수<span className="choose">(필수*)</span>
+                    현장 사진 접수<span className="choose" style={{color:'#3ac47d'}}>(필수*)</span>
                 </div>
                 <input type="file" id="takePicture" name="picture" accept="image/*" style={{display:'none'}}/>
                 <label htmlFor="takePicture"> {/*input태그 이미지로 받기 위함*/}
                     <img src="../../picture/orangecam.png" id="cam" alt="cam picture" onClick={Show} style={styleCamImg1}/>
                 </label>
-                <img id="show1" style={styleImgShow1}/>
+                <div style={styleImgShow1}>
+                    <img id="show1" />
+                    <br /> <br />
+                    <div className="adjust">*얼굴은 모자이크 처리됩니다.</div>
+                </div>
             </div>
-            <br /><br />
+            <br /><br /><br />
         </div>
 
         <div className="box" style={styleMap}> {/*위치, 지도, 불편내용 접수, 등록버튼*/}
@@ -432,13 +434,20 @@ function Home(){
             <div className="adjust">
                 &nbsp;&nbsp;&nbsp;&nbsp;*위치가 틀릴 경우 주소를 입력 또는 지도에서 위치를 조정해주세요.
             </div>
-            <br /><br /><br />
+            <br /><br /><br /><br />
             <div className="boxgroup">
-                <div className="write" style={styleColor}>
-                    불편 내용 접수<span className="choose">(선택)</span>
-                </div>
+                <span style={styleCam2}>
+                    <div className="write" style={styleColor}>
+                        불편 내용 접수<span className="chooseReport" style={{color:'#3ac47d'}}>(*필수)</span>
+                    </div>
+                </span>
+                <span style={styleCam1}>
+                    <div className="write" style={styleColor}>
+                        불편 내용 접수<span className="choose">(선택)</span>
+                    </div>
+                </span>
                 <textarea rows="10" id="text" name="text" onKeyUp={textCheck} placeholder="불편 내용을 입력해주세요." style={styleBorder}></textarea>
-                <br /><br /><br /><br />
+                <br /><br /><br /><br /><br />
 
                 {/* 재난 위험 사진 신고 - camera2 */}
                 <div style={styleCam2}>
@@ -449,7 +458,11 @@ function Home(){
                     <label htmlFor="takePicture"> {/*input태그 이미지로 받기 위함*/}
                         <img src="../../picture/cam.png" id="cam" alt="cam picture" onClick={Show} style={styleCamImg2}/>
                     </label>
-                    <img id="show2" style={styleImgShow2}/>
+                    <div style={styleImgShow2}>
+                        <img id="show2" />
+                        <br /> <br />
+                        <div className="adjust">*얼굴은 모자이크 처리됩니다.</div>
+                    </div>
                     <br /><br /><br />
                 </div>
 
